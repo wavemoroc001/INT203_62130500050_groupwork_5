a4a9ed4ffffcb5d36f8f6ac89f4a4f36b6568ca3
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-02-27 15:37:25 +0700

    [Fix] photoview and optimize togglelike()

--- 62130500050_groupwork_5/components/PhotoView.js
+++ 62130500050_groupwork_5/components/PhotoView.js
@@ -1,10 +1,10 @@
 app.component('photo-view', {
     props: {
-        isZoom: {
+        iszoom: {
             type: Boolean,
             require: true
         },
-        current: {
+        current_img: {
             type: String,
             require: true
         }
@@ -12,13 +12,13 @@ app.component('photo-view', {
     template:
         /*html*/
         `
-        <div class="bg-black min-h-screen w-full flex justify-center fixed left-0 top-0 bg-opacity-80" v-if="isZoom">
+        <div class="bg-black min-h-screen w-full flex justify-center fixed left-0 top-0 bg-opacity-80" v-if="iszoom">
             <div class="container flex flex-col h-full justify-center  mt-10">
                 <div class="flex items-center justify-end cursor-pointer">
                     <i v-on:click="zoomOutImages" class="fas fa-window-close text-white text-5xl"></i>
                 </div>
                 <div class="bg-red-300 w-8/12  ml-24">
-                    <img :src="current" >
+                    <img :src="current_img" >
                 </div>
             </div>
         </div>
--- 62130500050_groupwork_5/main.js
+++ 62130500050_groupwork_5/main.js
@@ -5,34 +5,28 @@ const app = Vue.createApp({
                 title: 'Emeral Lake State Park, USA',
                 src: './images/Emeral_Lake.jpg',
                 detail: 'Emerald Lake State Park is a 430-acre state park in East Dorset, Vermont, USA. It is the home of 20-acre Emerald Lake, called such because of its emerald color.Activities includes swimming, non-motorized boating, camping, fishing, hiking, picnicking, bicycling, wildlife watching, and winter sports.',
-
                 like: false,
-                dislike: true,
                 showInfo: false
             },
             {
                 title: 'Floras Lake, USA',
                 src: './images/Floras_Lake.jpg',
                 detail: 'Floras Lake is a natural 236-acre (96 ha) body of water on the southern Oregon Coast of the United States. Fed by four small tributaries from a basin of about 10 square miles (26 km2) in Curry County, it lies about 10 miles (16 km) north of Port Orford and 2 miles (3 km) west of U.S. Route 101.[1] It was probably named for Fred Flora, a 19th-century settler and miner who lived nearby.',
-
                 like: false,
-                dislike: true,
                 showInfo: false
             },
             {
                 title: 'Ninh Binh, Vietname',
                 src: './images/Ninh_Binh_Vietnam.jpg',
                 detail: 'Ninh Binh is also known as the Ha Long Bay on land for an obvious reason – it looks like Ha Long Bay but on land instead of water. There are hundreds of limestone monoliths topped by dense greenery that emerge from the ground, scattered around the city with rivers flowing through them and caves formed in them. While the mountains are in the west and northwest, the east and south have coastal parts. Ninh Binh really does have everything.',
-
                 like: false,
-                dislike: true,
                 showInfo: false
             },
             {
                 title: 'Oak Alley, USA',
                 src: './images/Oak_Alley_Us.jpg',
                 detail: 'Oak Alley Plantation is a historic plantation located on the west bank of the Mississippi River, in the community of Vacherie, St. James Parish, Louisiana, U.S. Oak Alley is named for its distinguishing visual feature, an alley (French allée) or canopied path, created by a double row of southern live oak trees about 800 feet (240 meters) long, planted in the early 18th century — long before the present house was built. The allée or tree avenue runs between the home and the River. The property was designated a National Historic Landmark for its architecture and landscaping, and for the agricultural innovation of grafting pecan trees, performed there in 1846–47 by an enslaved gardener.',
-                dislike: true,
+                like: false,
                 showInfo: false
             },
             {
@@ -40,7 +34,6 @@ const app = Vue.createApp({
                 src: './images/Skagit_Valley_Us.jpg',
                 detail: 'The Skagit Valley lies in the northwestern corner of the state of Washington, United States. Its defining feature is the Skagit River, which snakes through local communities which include the seat of Skagit County, Mount Vernon, as well as Sedro-Woolley, Concrete, Lyman-Hamilton, and Burlington.',
                 like: false,
-                dislike: true,
                 showInfo: false
             }
             ],
@@ -54,17 +47,13 @@ const app = Vue.createApp({
     methods: {
         toggleShow(index) {
             this.searchPic[index].showInfo = !this.tasks[index].showInfo
-            //console.log(`ShowInfo : ${this.tasks[index].showInfo}`)
         },
         toggleLike(index) {
-            this.searchPic[index].dislike = !this.tasks[index].dislike
             this.searchPic[index].like = !this.tasks[index].like
-            //console.log(`${index} \n  LIKE : ${this.tasks[index].like} \n DISLIKE : ${this.tasks[index].dislike}`);
         },
         cancleSearch(){
             this.searchWord = " " 
             this.toggelShowSearchTab() 
-            //console.log(this.searchWord)
         },
         toggelShowSearchTab(){
             this.searchTab = !this.searchTab
